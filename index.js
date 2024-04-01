e840c703fd1f768ec42d67980be6c890a3410172
Dhirazcode <dhiraj@academiacollege.edu.np>
2024-04-01 12:55:22 +0545

    random function

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const input=document.getElementById("wordtype");
 const container=document.querySelector(".container");
 
-let numofwords;
+const generate=(n)=>{
+    const letters="abcdefghijklmnop";
+    let text="";
+
+    for(let i=0;i<n;++i){
+        const random =(Math.random()*15).toFixed(0);
+        text += letters[random];
+        console.log(random);
+    }
+    return text;
+    // return (Math.random()*10).toFixed(0);
+}
+console.log(generate(9));
+
 const generatepara = () => {
-     numofwords = Number(input.value);
+     const wordtype = Number(input.value);
 
     const para = document.createElement("p");
 
-    para.setAttribute("class","paras");
-    para.innerText = ("sample para");
+    para.setAttribute("class", "paras");
 
     container.prepend("para");
+    para.innerText = "sample para";
 }
