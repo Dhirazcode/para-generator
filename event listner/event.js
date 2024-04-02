36a094eb87af4301e4f5c935572aae3af988cbe2
Dhirazcode <dhiraj@academiacollege.edu.np>
2024-04-03 00:43:19 +0545

    math functon

--- event listner/event.js
+++ event listner/event.js
@@ -2,24 +2,27 @@ const welcome=document.getElementById("welcome");
 const box1=document.getElementById("box1");
 const clickme=document.getElementById("clickme");
 
-clickme.addEventListener("click",()=>{
-    
-    welcome.classList.add("hello");
-});
 
-clickme.addEventListener("contextmenu",(e)=>{
+clickme.addEventListener("click",(e)=>{
     clickme.style.backgroundColor="purple";
     e.preventDefault(); 
-})
+    clickme.removeEventListener("click")
+});
+function myfunc(){
+    welcome.classList.add("hello");
+}
+console.log("click",myfunc);
+
 
 box1.addEventListener("mouseover",()=>{
     // welcome.remove();
+    clickme.style.backgroundColor="black";
     box1.style.backgroundColor="cyan";
     welcome.style.transform="scale(1.1)";
 })
 
 welcome.addEventListener("mousemove",()=>{
-   box1.style.backgroundColor="white";
+   box1.style.backgroundColor="green";
 })
 
 // addEventListener("contextmenu", (e) => {
@@ -28,7 +31,7 @@ welcome.addEventListener("mousemove",()=>{
 //     clickme.style.filter="blur(10px)";
 // });
 
-addEventListener("click",(e)=>{
-    console.log(e.clientX);
-    console.log(e.clientY)
-})
+// addEventListener("click",(e)=>{
+//     console.log(e.clientX);
+//     console.log(e.clientY)
+// })
---
+++ event listner/math object and date/mathandobject.js
@@ -0,0 +1,17 @@
+// const a = Math.random(4.5);
+// const a =Math.floor(4.8);
+// const a = Math.abs(40.55585);
+const b= (90*Math.PI)/180;
+const a = Math.sin(b);
+console.log(a);
+
+const math=(a,b)=>{
+    return a-b;
+}
+console.log(math(8,5));
+
+const m=Math.log2(16);
+console.log(m);
+
+const ma=Math.floor(Math.random()*10+5);
+console.log(ma);
