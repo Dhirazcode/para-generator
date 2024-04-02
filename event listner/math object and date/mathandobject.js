55817417f814739df562dc303e7d0537a834af60
Dhirazcode <dhiraj@academiacollege.edu.np>
2024-04-03 01:16:26 +0545

    dates

--- event listner/math object and date/mathandobject.js
+++ event listner/math object and date/mathandobject.js
@@ -1,17 +1,43 @@
 // const a = Math.random(4.5);
 // const a =Math.floor(4.8);
 // const a = Math.abs(40.55585);
-const b= (90*Math.PI)/180;
-const a = Math.sin(b);
-console.log(a);
+// const b= (90*Math.PI)/180;
+// const a = Math.sin(b);
+// console.log(a);
 
-const math=(a,b)=>{
-    return a-b;
-}
-console.log(math(8,5));
+// const math=(a,b)=>{
+//     return a-b;
+// }
+// console.log(math(8,5));
 
-const m=Math.log2(16);
-console.log(m);
+// const m=Math.log2(16);
+// console.log(m);
 
-const ma=Math.floor(Math.random()*10+5);
-console.log(ma);
+// const ma=Math.floor(Math.random()*10+5);
+// console.log(ma);
+
+// let n=Math.pow(100,2);
+// console.log(n);
+
+// let squreroot=Math.sqrt(25);
+// console.log(squreroot);
+
+// const cube=(a,b,c)=>{
+ 
+//     return a*b*c;
+// }
+// console.log("the cube of",cube(3,3,3));
+
+// const random= Math.sin((90*Math.PI)/180);
+// console.log(random);
+
+
+//date
+const date =new Date();
+console.log(date);
+console.log(date.getDay());
+console.log(date.setHours(45));
+console.log(date.getFullYear());
+
+const dates=new Date("2002 02 05");
+console.log(dates)
