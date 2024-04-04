806961aaecf2cc72773f774aea202ad7730fdc6f
Dhirazcode <dhiraj@academiacollege.edu.np>
2024-04-04 14:25:50 +0545

    update

--- event listner/localstroge/timeout and time interval/settimeout.js
+++ event listner/localstroge/timeout and time interval/settimeout.js
@@ -1,21 +1,24 @@
 const btn=document.querySelector("button")
 
-const printme=(name , income , gender) => {
-    console.log(`welcome ${name} .your income is ${income} and your gender is${gender}`);
+const printme=(name , gender) => {
+    const income=Math.floor(Math.random()*10000);
+    // console.log(`welcome ${name} .your income is ${income} and your gender is${gender}`);
+    console.log(new Date(Date.now()));
 };
-const print=(name , income , gender) => {
-    console.log(`welcome ${name} .your income is ${income} and your gender is${gender}`);
-};
+// const print=(name ,gender) => {
+//     const income=Math.floor(Math.random()*10000);
+//     console.log(`welcome ${name} .your income is ${income} and your gender is${gender}`);
+// };
 //timout
-const timeout= setTimeout(printme,1000,"dhiraj",2000,"male");
+// const timeout= setTimeout(printme,1000,"dhiraj","female");
 
 //timeinterval
-const timeinterval= setInterval(print,1000,"dhiraj",2000,"male");
+const timeinterval= setInterval(printme,1000,"dhiraj","male");
 
 
-btn.addEventListener("click",()=>{
-    clearTimeout(timeout);
-});
+// btn.addEventListener("click",()=>{
+//     clearTimeout(timeout);
+// });
 
 btn.addEventListener("click",()=>{
     clearTimeout(timeinterval);
