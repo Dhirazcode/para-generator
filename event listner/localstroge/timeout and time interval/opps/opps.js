67ffc36a9c7d041ade5b2d87099a7e2011f6dc55
Dhirazcode <dhiraj@academiacollege.edu.np>
2024-04-04 23:32:40 +0545

    constructor function

--- event listner/localstroge/timeout and time interval/opps/opps.js
+++ event listner/localstroge/timeout and time interval/opps/opps.js
@@ -9,19 +9,32 @@
 // mobilelegendheros.quote();
 
 // factory function
-function hero(a,b,price){
-    const temp = {
-        name: a,
-        language: b,
-        amount: ()=> {
-            console.log(price.value);
-        },
-        // price:price,        
-    };
-    return temp;
-}
-const mobilelegends=hero("zilog","nepali",32000);
-const mobilelegend=hero("alfa","hindi",32000);
+// function hero(a,b,price){
+//     const temp = {
+//         name: a,
+//         language: b,
+//         amount: ()=> {
+//             console.log(price);
+//         },
+//         // price:price,        
+//     };
+//     return temp;
+// }
+// const mobilelegends=hero("zilog","nepali",32000);
+// const mobilelegend=hero("alfa","hindi",32000);
 
-console.log(mobilelegends);
-console.log(mobilelegend);
+// console.log(mobilelegends);
+// console.log(mobilelegend);
+
+
+
+//constructor finction
+const star=function(name,gender,type) {
+    this.username = name;
+    this.sex= gender;
+    this.role=type;
+}
+const herosofmlbb = new star("zilong","male","fighter");
+const mlbb=new star("darroth","male","fighter");
+console.log(mlbb)
+console.log(herosofmlbb);
