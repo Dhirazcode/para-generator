7adcd354d2e09d5fc7020895b9b5414c7fa9973c
Dhirazcode <dhiraj@academiacollege.edu.np>
2024-04-08 12:44:17 +0545

    update getter and setter function

--- event listner/localstroge/timeout and time interval/opps/classes and inheritances/classes.js
+++ event listner/localstroge/timeout and time interval/opps/classes and inheritances/classes.js
@@ -22,21 +22,32 @@ class myclass{
     get getdata(){
      return this.#b;   
     }
-    set setdata(a){
+    set SetdataA(a){
         this.#a=a;
     }
-    set setdata1(b){
+    set SetdataB(b){
         this.#b=b;
     }
     hello(){
         console.log("welcome to my page",this.#a);
-    }
-}
-const obj=new myclass(220,"dhiraj");
-obj.hello();
-console.log(obj.getdata)
-console.log(obj)
-obj.setdata="helloworld";
-obj.setdata1="helloworld!";
+    };
+};
+const obj=new myclass(220,500);
+// obj.hello();
 
-console.log(obj);
+
+
+console.log(obj.getdata);
+
+const seta=obj.SetdataA = 100;
+console.log(seta)
+
+const setb=obj.SetdataB = 1000;
+console.log(setb);
+
+
+
+
+
+
+
