f90cf6f722521dc2f508555a797170303ec44eb9
Dhirazcode <dhiraj@academiacollege.edu.np>
2024-04-08 22:53:50 +0545

    Asynchronous

---
+++ event listner/localstroge/timeout and time interval/opps/classes and inheritances/Asynchronous/Asynchronous.js
@@ -0,0 +1,18 @@
+// function myclass(name){
+//     setTimeout(() => {
+//         for(let i=1;i<=100;i++){
+//             console.log(`my name is ${name}: ${i}`)
+//         }
+//     }, 5000);
+
+// }
+// myclass("dhiraj");
+// alert("welcome");
+
+
+setTimeout(() => {
+    alert(" lets move on i am done")
+}, 3000);
+for(let i=1;i<=100;i++){
+                console.log(`my name is Dhiraj ${i}`)
+            }
