b5cc8ef2675fa09c7db2918fba8726eeba7dc72e
Dhirazcode <dhiraj@academiacollege.edu.np>
2024-04-09 13:59:33 +0545

    updates

--- event listner/localstroge/timeout and time interval/opps/classes and inheritances/Asynchronous/callback and callbackhell/Promises/Promises.js
+++ event listner/localstroge/timeout and time interval/opps/classes and inheritances/Asynchronous/callback and callbackhell/Promises/Promises.js
@@ -1,18 +1,43 @@
-const a=new Promise((resolve,reject)=>{
-    let success=true;
-    if(success){
-        resolve("promises is fulffilled")
-    }
-    else{
+// const a=new Promise((resolve,reject)=>{
+//     let success=true;
+//     if(success){
+//         resolve("promises is fulffilled")
+//     }
+//     else{
     
-        reject("some technical error")
-    }
-})
+//         reject("some technical error")
+//     }
+// })
 
-a.then((parameter)=>{
+// a.then((parameter)=>{
 
-    console.log(parameter)
-}).catch((error)=>{
+//     console.log(parameter)
+// }).catch((error)=>{
 
-    console.log(error)
-})
+//     console.log(error)
+// })
+
+const arr=[];
+
+const fetchdata=(arr=[])=>{
+    return new Promise((resolve,reject)=>{
+
+        setTimeout(() => {
+            arr.push({
+                name:"dhiraj",
+                id:100,
+            });
+            if(arr.length>0){
+                resolve("data is fetched")
+            }
+            else{
+                reject("Due to some technical error")
+            }
+        }, 4000);
+    })
+}
+fetchdata()
+.then((message)=>console.log(message))
+.then(()=>console.log("dhiraj"))
+.catch((message)=>console.log(message))
+.finally(()=>console.log("find another way to completed"))
