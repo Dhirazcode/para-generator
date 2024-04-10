aae6e20e1248e356f73345e1e3b9a5445058f760
Dhirazcode <dhiraj@academiacollege.edu.np>
2024-04-10 15:26:27 +0545

    bom

---
+++ event listner/localstroge/timeout and time interval/opps/classes and inheritances/Asynchronous/callback and callbackhell/Promises/fetch api/async and wait/bom/vom.js
@@ -0,0 +1,15 @@
+// navigator.geolocation.getCurrentPosition((e)=>{
+//     console.log(e)
+
+// },
+// (d)=>console.log(d)
+// )
+
+console.log(location.host);
+console.log(location.pathname);
+console.log(location.hostname);
+console.log(location.port);
+
+
+
+console.log(location.href)
